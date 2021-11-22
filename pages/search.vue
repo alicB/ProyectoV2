72923c4916cb129c516b588e004d5286ed614299
Cristobal2697 <37197715+Cristobal2697@users.noreply.github.com>
2021-11-22 13:52:22 -0600

    Add files via upload

---
+++ pages/search.vue
@@ -0,0 +1,31 @@
+<style>
+
+.ret{
+  font-size: 12pt;
+  text-decoration: none;
+  border:1px solid black;
+  border-radius: 7px;
+  padding:8px;
+  margin-top: 20px;
+  color: black;
+  font-weight: bold;
+}
+
+.t{
+  padding-top: 20px;
+}
+
+.p{
+  padding-top: 20px;
+}
+</style>
+
+<template>
+  <div class="container">
+   <div class="row p">
+    <Nuxt-link to="/" class="ret">HOME</Nuxt-link>
+    <h3 class="t">Search Page</h3>
+	  <AppSearchInput />
+   </div>
+  </div>
+</template>
